3a5cba30a5749a8f8c40e93ca1e7cc7ee3210ae9
UunizumehValentin <valentineuunizumehnuobetah77@gmail.com>
2021-04-20 08:35:48 -0700

    added cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const connectDB = require("./config/connectDB");
 const traineesRoute = require("./routes/traineesRoute");
 const usersRoute = require("./routes/usersRoute");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ connectDB();
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 // routes
 app.use("/api/v1/trainees", traineesRoute);
